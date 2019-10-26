80764ef6f205b9f7a27e3c4533497a7eeb86e245
Ilya Naydanov <turbcool@gmail.com>
2019-10-26 23:50:40 +0500

    lab 2 start: TODO

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+import random
 import math
 from enum import Enum
 
@@ -29,69 +30,108 @@ class Weather:
         return self.toString() + '||Евклид: ' + str("%.3f" % self.dEuq) + '|Хемминг: ' + str("%.3f" % self.dHem) + '|Город. кварт.: ' + str("%.3f" % self.dCity)
 
 
-# Исходные данные:
-wData = [
-    Weather('26 окт', 8, 0.4, 7, 77, Quality.very_good),
-    Weather('27 окт', 7, 10.7, 9, 93, Quality.bad),
-    Weather('28 окт', 1, 11.5, 5, 87, Quality.bad),
-    Weather('29 окт', -5, 0, 3, 72, Quality.very_good),
-    Weather('30 окт', -4, 0, 3, 67, Quality.very_good),
-    Weather('31 окт', -3, 1.3, 4, 71, Quality.good),
-    Weather('1 нояб', -6, 1.8, 5, 72, Quality.good),
-    Weather('2 нояб', -3, 7.6, 2, 89, Quality.very_bad),
-    Weather('3 нояб', -11, 0, 3, 82, Quality.good),
-    Weather('4 нояб', -4, 4.2, 6, 82, Quality.bad)
-]
+def clasterize(wList):
+    clusters = []
+    # 1. Берём случайный центр:
+    i = random.randint(0, len(wList))
+    center = wList[i]
+    clusters.append(center)
+    wList.pop(i)
 
-# Ввод погоды для классификации:
-print('Введите погоду для проверки:')
-print('Температура (гр.):')
-t = input()
-print('Количество осадков (мм):')
-fall = input()
-print('Скорость ветра (м/с):')
-wind = input()
-print('Влажность (%):')
-humid = input()
-wUser = Weather('?', t, fall, wind, humid)
+    # 2. Ищем образ с наибольшим расстоянием:
+    for w in wList:
+        w.dEuq = euqlid(wList, center)
+    maxEuq = max(wList, key=lambda x: x.dEuq)
+    maxEuqIndex = wList.index(maxEuq)
+    # 2.1 Переносим его в массив кластеров:
+    clusters.append([maxEuq])
+    wList.pop(maxEuqIndex)
 
-# Вычисление расстояний:
-for w in wData:
-    w.dEuq = math.sqrt((wUser.temp - w.temp) ** 2 + (wUser.fall - w.fall)
-                       ** 2 + (wUser.wind - w.wind) ** 2 + (wUser.humid - w.humid) ** 2)
-    w.dHem = abs(wUser.temp - w.temp) + abs(wUser.fall - w.fall) + \
-        abs(wUser.wind - w.wind) + abs(wUser.humid - w.humid)
-    w.dCity = max([abs(wUser.temp - w.temp), abs(wUser.fall - w.fall),
-                   abs(wUser.wind - w.wind), abs(wUser.humid - w.humid)])
 
-minEuq = wData[0]
-minHem = wData[0]
-minCity = wData[0]
+def euqlid(w1, w2):
+    return math.sqrt((w1.temp - w2.temp) ** 2 + (w1.fall - w2.fall)
+                     ** 2 + (w1.wind - w2.wind) ** 2 + (w1.humid - w2.humid) ** 2)
 
-for w in wData:
-    if minEuq.dEuq > w.dEuq:
-        minEuq = w
-    if minHem.dHem > w.dHem:
-        minHem = w
-    if minCity.dCity > w.dCity:
-        minCity = w
 
-# Вывод результатов:
-print('___Метод Евклида___')
-print('Класс: ' + minEuq.quality.value)
-print('Расстояние: ' + str("%.3f" % minEuq.dEuq))
-print('Ближайший сосед: ' + minEuq.date)
+def hemming(w1, w2):
+    return abs(w1.temp - w2.temp) + abs(w1.fall - w2.fall) + abs(w1.wind - w2.wind) + abs(w1.humid - w2.humid)
+
+
+def city(w1, w2):
+    return max([abs(w1.temp - w2.temp), abs(w1.fall - w2.fall), abs(w1.wind - w2.wind), abs(w1.humid - w2.humid)])
+
+
+task = -1
+
+print('Введите номер задания:')
+print('1. Классификация')
+print('2. Кластеризация')
+print('3. Метод голосования')
+print('0. Завершить программу')
 print()
-print('___Метод Хемминга___')
-print('Класс: ' + minHem.quality.value)
-print('Расстояние: ' + str("%.3f" % minHem.dHem))
-print('Ближайший сосед: ' + minHem.date)
-print()
-print('___Метод городских кварталов___')
-print('Класс: ' + minCity.quality.value)
-print('Расстояние: ' + str("%.3f" % minCity.dCity))
-print('Ближайший сосед: ' + minCity.date)
-print()
-print('Расстояния: ')
-for w in wData:
-    print(w.toStringWithDistance())
+
+task = input()
+
+while (task != 0):
+    # Исходные данные:
+    wData = [
+        Weather('26 окт', 8, 0.4, 7, 77, Quality.very_good),
+        Weather('27 окт', 7, 10.7, 9, 93, Quality.bad),
+        Weather('28 окт', 1, 11.5, 5, 87, Quality.bad),
+        Weather('29 окт', -5, 0, 3, 72, Quality.very_good),
+        Weather('30 окт', -4, 0, 3, 67, Quality.very_good),
+        Weather('31 окт', -3, 1.3, 4, 71, Quality.good),
+        Weather('1 нояб', -6, 1.8, 5, 72, Quality.good),
+        Weather('2 нояб', -3, 7.6, 2, 89, Quality.very_bad),
+        Weather('3 нояб', -11, 0, 3, 82, Quality.good),
+        Weather('4 нояб', -4, 4.2, 6, 82, Quality.bad)
+    ]
+
+    # Ввод погоды для классификации:
+    print('Введите погоду для проверки:')
+    print('Температура (гр.):')
+    t = input()
+    print('Количество осадков (мм):')
+    fall = input()
+    print('Скорость ветра (м/с):')
+    wind = input()
+    print('Влажность (%):')
+    humid = input()
+    wUser = Weather('?', t, fall, wind, humid)
+
+    print('Введённая погода:')
+    print(wUser.toString())
+
+    if (task == '1'):
+        # Вычисление расстояний:
+        for w in wData:
+            w.dEuq = euqlid(w, wUser)
+            w.dHem = hemming(w, wUser)
+            w.dCity = city(w, wUser)
+
+        minEuq = min(wData, key=lambda x: x.dEuq)
+        minHem = min(wData, key=lambda x: x.dHem)
+        minCity = min(wData, key=lambda x: x.dCity)
+
+        # Вывод результатов:
+        print('___Метод Евклида___')
+        print('Класс: ' + minEuq.quality.value)
+        print('Расстояние: ' + str("%.3f" % minEuq.dEuq))
+        print('Ближайший сосед: ' + minEuq.date)
+        print()
+        print('___Метод Хемминга___')
+        print('Класс: ' + minHem.quality.value)
+        print('Расстояние: ' + str("%.3f" % minHem.dHem))
+        print('Ближайший сосед: ' + minHem.date)
+        print()
+        print('___Метод городских кварталов___')
+        print('Класс: ' + minCity.quality.value)
+        print('Расстояние: ' + str("%.3f" % minCity.dCity))
+        print('Ближайший сосед: ' + minCity.date)
+        print()
+        print('Расстояния: ')
+        for w in wData:
+            print(w.toStringWithDistance())
+
+    if task == 2:
+        clasterize(wData, wUser)
